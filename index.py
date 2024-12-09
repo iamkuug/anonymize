df02ec430d69d21247cd3b84b1d1807e6cfe8593
iamkuug <jonathan.kuug@ic.africa>
2024-12-09 07:57:05 +0000

    refactor: new config.yml structure + running test seeder

--- index.py
+++ index.py
@@ -1,55 +1,94 @@
 import yaml
-import pymysql
+import mysql.connector
 import os
-import json
 import logging
-
+import json
 from dotenv import load_dotenv
-from setup import seed
-from utils import shift, mask, op
+from utils import Masker, TestSeeder
 
-#Load env
-load_dotenv()
+class Anonymizer:
+    @staticmethod
+    def load_config():
+        with open("config.yml", "r") as file:
+            return yaml.safe_load(file)
 
-#Load configuration file
-with open("config.yml", "r") as file:
-    config = yaml.safe_load(file)
-
-db_flavour = os.getenv("DATABASE_FLAVOUR")
-conn = None
-
-try:
-    if db_flavour == "mysql":
-        conn = pymysql.connect(
-            host = os.getenv("DATABASE_HOST"),
-            user = os.getenv("DATABASE_USER"),
-            password = os.getenv("DATABASE_PASSWORD"),
-            database= os.getenv("DATABASE_NAME"),
-            charset='utf8mb4',
-            cursorclass=pymysql.cursors.DictCursor
-        )
-    else:
-        logging.error("Database flavour not supported")
-except:
-    logging.error("Error connecting to database")
-
-def anonymize_data(table_name, column_op_mappings, batch_size=100):
-    with conn.cursor() as cursor:
-        columns = ",".join(column_op_mappings.keys())
-        sql = f"SELECT {columns} FROM {table_name}"
-        cursor.execute(sql)
-
-        result = cursor.fetchall()
-
-
-    conn.commit()
-
-
-def main():
-    seed()
-    for table_name, column_op_mappings in config["tables"].items():
-        anonymize_data(table_name, column_op_mappings) 
+    @staticmethod
+    def connect_to_db(dialect):
+        if dialect == "mysql":
+            return mysql.connector.connect(
+                host=os.getenv("DATABASE_HOST"),
+                user=os.getenv("DATABASE_USER"),
+                password=os.getenv("DATABASE_PASSWORD"),
+                database=os.getenv("DATABASE_NAME")
+            )
+        else:
+            logging.error("Database dialect not supported. Add appropriate property to your config.yml")
+            return None
         
-main()
+    
+    @staticmethod
+    def apply_masking(mask_type, value):
+        if mask_type == 'email':
+            return Masker.mask_email(value)
+        elif mask_type == 'phone':
+            return Masker.mask_phone(value)
+        elif mask_type == 'password':
+            return Masker._hash_value(value)
+        elif mask_type == 'address':
+            return Masker.mask_address(value)
+        elif mask_type == 'date':
+            return Masker.mask_date(value)
+        else:
+            logging.error(f"Unknown mask type: {mask_type}")
+            return value
 
+    @staticmethod
+    def anonymize_data(conn, table_name, column_meta, batch_size=100):
+        primary_key = column_meta["primary_key"] if column_meta.get("primary_key") else "id"
+        mask_columns = column_meta["mask_columns"]
+        column_op_mappings = {list(col.keys())[0]: list(col.values())[0] for col in mask_columns}
 
+        cursor = conn.cursor(dictionary=True)
+        columns = ",".join(column_op_mappings.keys())
+        offset = 0
+
+        while True:
+            sql = f"SELECT {primary_key}, {columns} FROM {table_name} LIMIT {batch_size} OFFSET {offset}"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+            if not result:
+                break
+
+            anonymized_result = [
+                {col: Anonymizer.apply_masking(column_op_mappings[col], row[col]) for col in column_op_mappings.keys()}
+                for row in result
+            ]
+
+            for row, anon_row in zip(result, anonymized_result):
+                set_clause = ", ".join([f"{col} = %s" for col in column_op_mappings.keys()])
+                update_sql = f"UPDATE {table_name} SET {set_clause} WHERE {primary_key} = %s"
+                cursor.execute(update_sql, [anon_row[col] for col in column_op_mappings.keys()] + [row[primary_key]])
+
+            conn.commit()
+            offset += batch_size
+
+        logging.debug(f"Anonymized {table_name} table")
+
+    @staticmethod
+    def run():
+        config = Anonymizer.load_config()
+        conn = Anonymizer.connect_to_db(config["dialect"])
+        table_data = config.get("tables")
+
+        if conn:
+            for table_name, column_meta in table_data.items():
+                Anonymizer.anonymize_data(conn, table_name, column_meta)
+            conn.close()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    load_dotenv()
+    seeder = TestSeeder()
+    seeder.run()
+    Anonymizer.run()
--- utils.py
+++ utils.py
@@ -1,17 +1,133 @@
-def hash():
-    pass
+import hashlib
+import mysql.connector
+import os
+import logging
 
-def mask():
-    pass
-
-def shift():
-    pass
+from mysql.connector import errorcode
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
 
+class Masker:
+    @staticmethod
+    def mask_phone(phone: str) -> str:
+        return f"{'*' * (len(phone) - 4)}{phone[-4:]}"
 
-op = {
-    'mask': mask,
-    'shift': shift
-}
+    @staticmethod
+    def mask_name(name: str) -> str:
+        return f"{name[0]}{'*' * (len(name) - 2)}{name[-1]}" if len(name) > 2 else name
+
+    @staticmethod
+    def mask_address(address: str) -> str:
+        parts = address.split()
+        if len(parts) > 1:
+            masked_parts = [parts[0]] + ['*' * len(part) for part in parts[1:]]
+            return ' '.join(masked_parts)
+        return address
+
+    @staticmethod
+    def mask_email(email: str) -> str:
+        local, domain = email.split('@')
+        return f"{local[0]}{'*' * (len(local) - 2)}{local[-1]}@{domain}" if len(local) > 2 else email
+
+    @staticmethod
+    def mask_date(date: str) -> str:
+        parts = date.split('-')
+        if len(parts) == 3:
+            return f"{parts[0]}-**-**"
+        return date
+
+    @staticmethod
+    def _hash_value(value: str) -> str:
+        return hashlib.sha256(value.encode()).hexdigest()[:10]  # Return first 10 characters of the hash
+
+class Shifter:
+    @staticmethod
+    def shift_date(date: str, shift_days: int) -> str:
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        shifted_date = date_obj + timedelta(days=shift_days)
+        return shifted_date.strftime("%Y-%m-%d")
+
+
+class TestSeeder:
+    def __init__(self):
+        
+        self.db_config = {
+            'user': os.getenv('DATABASE_USER'),
+            'password': os.getenv('DATABASE_PASSWORD'),
+            'host': os.getenv('DATABASE_HOST'),
+            'database': "anony"
+        }
+
+        print(self.db_config)
+        self.conn = None
+        self.cursor = None
+
+    def connect(self):
+        try:
+            self.conn = mysql.connector.connect(**self.db_config)
+            self.cursor = self.conn.cursor()
+            logging.debug("Connected to test database")
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                logging.critical("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                logging.warning("Database does not exist, creating database ...")
+                self.create_database()
+                self.connect()
+            else:
+                logging.error(err)
+
+    def create_database(self):
+        try:
+            temp_config = self.db_config.copy()
+            temp_config.pop('database')
+            self.conn = mysql.connector.connect(**temp_config)
+            self.cursor = self.conn.cursor()
+            self.cursor.execute(f"CREATE DATABASE {self.db_config['database']}")
+            logging.debug(f"Database {self.db_config['database']} created successfully")
+        except mysql.connector.Error as err:
+            logging.critical(f"Failed to create database: {err}")
+        finally:
+            self.disconnect()
+
+    def disconnect(self):
+        if self.conn:
+            self.cursor.close()
+            self.conn.close()
+            logging.debug("Connection to test database closed")
+
+    def wipe_data(self):
+        self.cursor.execute("SET FOREIGN_KEY_CHECKS = 0;")
+        self.cursor.execute("SHOW TABLES;")
+        tables = self.cursor.fetchall()
+        for table in tables:
+            self.cursor.execute(f"TRUNCATE TABLE {table[0]};")
+        self.cursor.execute("SET FOREIGN_KEY_CHECKS = 1;")
+        self.conn.commit()
+        logging.debug("All data wiped")
+
+    def seed_data(self):
+        self.cursor.execute("""
+            INSERT INTO users (id, email, phone, password, address) VALUES
+            (1, 'john.doe@example.com', '1234567890', 'password123', '123 Main St'),
+            (2, 'jane.doe@example.com', '0987654321', 'password456', '456 Elm St');
+        """)
+        self.cursor.execute("""
+            INSERT INTO orders (id, shipping_address) VALUES
+            (1, '123 Main St'),
+            (2, '456 Elm St');
+        """)
+        self.conn.commit()
+        logging.debug("Dummy data inserted")
+
+    def run(self):
+        self.connect()
+        self.wipe_data()
+        self.seed_data()
+        self.disconnect()
+    
+
+
 
 
